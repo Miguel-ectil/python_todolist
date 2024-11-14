de819801a01f05e6d9ff35c254eebd0abef6bc6e
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2024-11-14 08:44:40 -0300

    Create db_config.py
    [ADD] Adicionando o arquivo de conexão com o banco de dados

---
+++ db_config.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Conexão com o banco de dados SQLite
+DATABASE_URL = "sqlite:///todoList.db"
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
