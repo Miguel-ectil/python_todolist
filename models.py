9b7930137f23fb8cdf27158dc0fb22dd2e7eb1f0
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2024-11-16 01:03:23 -0300

    Update models.py
    [ADD] Adicionando a modelagem da primeira tabela a de usuário

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
 from sqlalchemy import Column, Enum, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from db_config import Base
+
+class Usuario(Base):
+  __tablename__ = 'usuarios'
