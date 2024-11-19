867cb782f9a856859e20db3baedec73009f17f87
Miguel Ectil <2301985@SALAS.AULAS>
2024-11-19 19:46:53 -0300

    [ADD] Adicionando colunas das tabelas Usuarios e Tarefas

--- models.py
+++ models.py
@@ -6,13 +6,22 @@ class Usuario(Base):
   __tablename__ = 'usuarios'
 
   id = Column(Integer, primary_key=True)
+  name = Column(String, unique= False nullable=False)
+  gmail = Column(String, unique= False nullable= True)
+  password = Column(String, unique=True, nullable=False)
+
 
   tarefas = relationship("Tarefa", back_populates="usuario")
 
 class Tarefa(Base):
   __tablename__ = 'tarefas
 
-  id = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True) 
+  titulo = Column(String, nullable=False)
+  descricao = Column(String)
+  status = Column(Enum('Pendente', 'Em_andamento', 'Finalizada', name='status_enum'), default='Pendente')
+  prioridade = Column(Enum('Baixa', 'Média', 'Alta', name='prioridade_enum'), default='Baixa') 
+
   id_usuario = Column(Integer, ForeignKey('usuarios.id'))
 
   usuario = relationship("Usuario", back_populates="tarefas")
